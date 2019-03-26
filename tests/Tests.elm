131558e775a33a4714f2d9e6ddcc074637b0bb58
Tadeusz Łazurski <tadeusz@lazurski.pl>
2019-03-26 12:52:59 +0100

    Tests: fuse 0 strength test against initial value and target
    Because the spring can jump to a resting position if the velocity and 
    distance is very small, we need to allow for some imprecision (< 0.05).

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,6 +1,6 @@
 module Tests exposing (suite)
 
-import Expect exposing (Expectation)
+import Expect exposing (Expectation, FloatingPointTolerance(..))
 import Fuzz exposing (Fuzzer, float, int, list, string)
 import Spring exposing (Spring)
 import Test exposing (..)
@@ -9,15 +9,29 @@ import Test exposing (..)
 suite : Test
 suite =
     describe "Springs"
-        [ fuzz float
+        [ let
+            fuzzer =
+                Fuzz.map3
+                    (\dampness target initial ->
+                        { dampness = dampness
+                        , target = target
+                        , initial = initial
+                        }
+                    )
+                    float
+                    float
+                    float
+          in
+          fuzz fuzzer
             "Spring with 0 strength won't move"
-            (\dampness ->
+            (\{ dampness, target, initial } ->
                 { strength = 0, dampness = dampness }
                     |> Spring.create
-                    |> Spring.jumpTo 100
+                    |> Spring.jumpTo initial
+                    |> Spring.setTarget target
                     |> Spring.animate 16
                     |> Spring.value
-                    |> Expect.equal 100
+                    |> Expect.within (Absolute 0.05) initial
             )
         , fuzz float
             "Spring at rest won't move"
