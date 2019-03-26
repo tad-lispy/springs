1345e07642c46c1739a84a6efc3253ed8d3b48a3
Tadeusz Łazurski <tadeusz@lazurski.pl>
2019-03-26 11:31:19 +0100

    First test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,10 +1,20 @@
 module Tests exposing (suite)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Fuzz exposing (Fuzzer, float, int, list, string)
+import Spring exposing (Spring)
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    fuzz float
+        "Spring with 0 force won't move"
+        (\dampness ->
+            { strength = 0, dampness = dampness }
+                |> Spring.create
+                |> Spring.jumpTo 100
+                |> Spring.animate 16
+                |> Spring.value
+                |> Expect.equal 100
+        )
