b17301205a5f9748bff78ac7a354e25fffdd35a3
Tadeusz Łazurski <tadeusz@lazurski.pl>
2019-03-26 11:55:53 +0100

    Tests: assert that spring at rest won't move

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -8,13 +8,24 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    fuzz float
-        "Spring with 0 force won't move"
-        (\dampness ->
-            { strength = 0, dampness = dampness }
-                |> Spring.create
-                |> Spring.jumpTo 100
-                |> Spring.animate 16
-                |> Spring.value
-                |> Expect.equal 100
-        )
+    describe "Springs"
+        [ fuzz float
+            "Spring with 0 strength won't move"
+            (\dampness ->
+                { strength = 0, dampness = dampness }
+                    |> Spring.create
+                    |> Spring.jumpTo 100
+                    |> Spring.animate 16
+                    |> Spring.value
+                    |> Expect.equal 100
+            )
+        , fuzz float
+            "Spring at rest won't move"
+            (\strength ->
+                { strength = strength, dampness = 0 }
+                    |> Spring.create
+                    |> Spring.animate 16
+                    |> Spring.value
+                    |> Expect.equal 0
+            )
+        ]
